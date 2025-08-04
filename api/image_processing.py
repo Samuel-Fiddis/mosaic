9a075629efd9ff2a7c1cf2266ab2e9a89cfafbb7
Sam Fiddis <sam.fiddis@gmail.com>
2025-08-04 13:50:22 +1000

    Added multiprocessing. Works better for large trees

--- api/image_processing.py
+++ api/image_processing.py
@@ -1,9 +1,9 @@
-from nearest_neighbour.nn import CIFAR_DATA, NNAlgorithm
-import numpy as np
+from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
-from PIL import Image
 
-from pprint import pprint
+import numpy as np
+from nearest_neighbour.nn import NNAlgorithm
+from PIL import Image
 
 IMAGE_PIXEL_SIZE = 32
 
@@ -22,17 +22,29 @@ def load_image_to_array(image_data):
     return img_array
 
 
+
+def _process_block(args):
+    i, j, block, algo = args
+    return (i, j, algo.query(block))
+
 def process_image(image_array, algo: NNAlgorithm):
     x, y = image_array.shape[:2]
     x = x - (x % IMAGE_PIXEL_SIZE)  # Ensure dimensions are multiples of 32
     y = y - (y % IMAGE_PIXEL_SIZE)  # Ensure dimensions are multiples of 32
-    print(x, y)
     new_image = np.zeros((x, y, 3), dtype=np.uint8)
+
+    tasks = []
     for i in range(0, x, IMAGE_PIXEL_SIZE):
         for j in range(0, y, IMAGE_PIXEL_SIZE):
-            new_image[i : i + IMAGE_PIXEL_SIZE, j : j + IMAGE_PIXEL_SIZE] = algo.query(
-                image_array[i : i + IMAGE_PIXEL_SIZE, j : j + IMAGE_PIXEL_SIZE]
-            )
+            block = image_array[i : i + IMAGE_PIXEL_SIZE, j : j + IMAGE_PIXEL_SIZE]
+            tasks.append((i, j, block, algo))
+
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(_process_block, tasks))
+
+    for i, j, processed_block in results:
+        new_image[i : i + IMAGE_PIXEL_SIZE, j : j + IMAGE_PIXEL_SIZE] = processed_block
+
     new_image = Image.fromarray(new_image)
     img_byte_arr = BytesIO()
     new_image.save(img_byte_arr, format="PNG")
